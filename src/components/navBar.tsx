0d2ac712289f5671e157aa380a5e59b2104bbe37
FinnCummins <finnkcummins@gmail.com>
2023-06-12 20:19:49 +0100

    modified:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   src/components/index.tsx 	new file:   src/components/navBar.tsx 	new file:   src/server/server.tsx

---
+++ src/components/navBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <div className="navbar">
+            <p><a href="index.html">Home</a> | </p>
+            <p><a href="about.html">About</a> | </p>
+            <p><a href="projects.html">Projects</a> | </p>
+            <p><a href="techblog.html">Tech blog</a></p>
+        </div>
+    );
+}
