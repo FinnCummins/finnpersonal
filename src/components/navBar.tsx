1f432f6ce64b147838c6d3bad87fcc865399b4f6
FinnCummins <finnkcummins@gmail.com>
2023-06-14 20:19:39 +0100

    Added a navbar to the left hand side of the page and replaced text links with emoticons

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faCode} from '@fortawesome/free-solid-svg-icons'
 
 export const NavBar = () => {
     return (
         <div className="navbar">
-            <p><a href="/">Home</a> | </p>
-            <p><a href="/about">About</a> | </p>
-            <p><a href="/projects">Projects</a> | </p>
-            <p><a href="/techblog">Tech blog</a></p>
+            <div id="nav">
+                <p><a href="/"><FontAwesomeIcon icon={faHome} color="#4d4d4e"/></a></p>
+                <p><a href="/about"><FontAwesomeIcon icon={faUser} color="#4d4d4e"/></a></p>
+                <p><a href="/projects"><FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/></a></p>
+                <p><a href="/techblog"><FontAwesomeIcon icon={faCode} color="#4d4d4e"/></a></p>
+            </div>
+            <div id="socials">
+                <p><a href="https://linkedin.com/in/finn-cummins" target="_blank" ><FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/></a></p>
+                <p><a href="https://github.com/finncummins" target="_blank"><FontAwesomeIcon icon={faGithub} color="#4d4d4e"/></a></p>
+            </div>
         </div>
     );
 }
