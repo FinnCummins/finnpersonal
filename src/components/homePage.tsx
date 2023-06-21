f683318104378d3b7cd4a5ae388952a04a740712
FinnCummins <finnkcummins@gmail.com>
2023-06-21 10:17:23 +0100

    Fixed the animation issue for my age in decimals. The animation for my one line bio on the home page is now working.

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -5,7 +5,14 @@ import AnimatedLetters from "./Animated Letters/AnimatedLetters";
 import "../assets/index.scss";
 
 export const Home = () => {
-    const [finnsAge, setFinnsAge] = useState((Date.now()-Date.parse("2002-09-02"))/1000/60/60/24/365.25);
+    const [finnsAge, setFinnsAge] = useState(
+        
+        ((Date.now()-Date.parse("2002-09-02"))/1000/60/60/24/365.25)
+        .toString()
+        .substring(0, 12)
+        .split("")
+    );
+
     const [letterClass, setLetterClass] = useState("text-animate");
     const lineOne = ["M", "y", " ", "n", "a", "m", "e", " ", "i", "s", " "];
     const firstName = ["F", "i", "n", "n"];
@@ -24,6 +31,7 @@ export const Home = () => {
                     (Date.now()-Date.parse("2002-09-02"))/1000/60/60/24/365.25)
                     .toString()
                     .substring(0,12)
+                    .split("")
             );
                 
         }, 250);
@@ -38,8 +46,8 @@ export const Home = () => {
             
             <div id="homePage-intro">
                 <h1>
-                    <AnimatedLetters letterClass={letterClass} strArray={lineOne} idx={1} /> <AnimatedLetters letterClass={letterClass} strArray={firstName} idx={11} /> <AnimatedLetters letterClass={letterClass} strArray={surname} idx={15} /> <AnimatedLetters letterClass={letterClass} strArray={lineTwo} idx={22} /> <div id="age">{finnsAge}</div> <AnimatedLetters letterClass={letterClass} strArray={lineThree} idx={28} /> 
-                    <AnimatedLetters letterClass={letterClass} strArray={lineFour} idx={46} /> <a href="https://en.wikipedia.org/wiki/Carlow" target="_blank"><AnimatedLetters letterClass={letterClass} strArray={lineFive} idx={50} /></a>
+                    <AnimatedLetters letterClass={letterClass} strArray={lineOne} idx={1} /> <AnimatedLetters letterClass={letterClass} strArray={firstName} idx={11} /> <AnimatedLetters letterClass={letterClass} strArray={surname} idx={15} /> <AnimatedLetters letterClass={letterClass} strArray={lineTwo} idx={22} /> <AnimatedLetters letterClass={letterClass} strArray={finnsAge.slice(0,7)} idx={28} /><span id="age">{finnsAge.slice(7, 12)}</span><br /> <AnimatedLetters letterClass={letterClass} strArray={lineThree} idx={40} /> 
+                    <AnimatedLetters letterClass={letterClass} strArray={lineFour} idx={60} /> <a href="https://en.wikipedia.org/wiki/Carlow" target="_blank"><AnimatedLetters letterClass={letterClass} strArray={lineFive} idx={64} /></a>
                 </h1>
             </div>
         
