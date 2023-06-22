e92dd3f79549a93fded2bd9b4e4fd4b6afef5b3f
FinnCummins <finnkcummins@gmail.com>
2023-06-22 10:04:55 +0100

    Removed gsap file from repository

--- src/components/Animated Letters/AnimatedLetters.tsx
+++ src/components/Animated Letters/AnimatedLetters.tsx
@@ -1,14 +1,15 @@
 import "./AnimatedLetters.scss"
 
 const AnimatedLetters = ({letterClass, strArray, idx}) => {
+
     return (
         <span>
             {
                 
                 strArray.map((letter, i) => (
-                    <span key={letter + i} className={`${letterClass} _${i + idx}`}>
-                        {letter}
-                    </span>
+                        <span key={letter + i} className={`${letterClass} _${i + idx}`}>
+                            {letter}
+                        </span>
                 ))
             }
         </span>
