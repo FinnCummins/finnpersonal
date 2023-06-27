1ddcb1fa7b64211384b54e85d512552889be8c36
FinnCummins <finnkcummins@gmail.com>
2023-06-27 10:46:16 +0100

    Removed console.log from techBlog component

--- src/components/techblog/techBlog.tsx
+++ src/components/techblog/techBlog.tsx
@@ -4,18 +4,19 @@ import  Preview  from "../Blog preview/preview";
 import { slugs } from "../../loadPosts.js"
 import { Outlet } from 'react-router-dom';
 
-console.log(slugs);
-
 export const TechBlog = () => {
+    console.log('Slugs:', slugs); 
     return (
         <>
             <NavBar />
             <div id="blog-layout">
                 <h2 id="techblog-header">Tech Blog</h2>
                 <div id="blogs-container">
-                    {slugs.map((slug) => (
-                        <Preview key={"dogpatch"} slug={"dogpatch"} />
-                    ))}
+                    {
+                        slugs.map((slug) => (
+                            <Preview key={"dogpatch"} slug={"dogpatch"} />
+                        ))
+                    }
                 </div>
             </div>
             <Outlet />
